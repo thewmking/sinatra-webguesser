7c429957fa857a62cae1d43a612a185d290bb71d
Matthew King <thewmking@gmail.com>
2017-03-16 15:30:22 -0400

    Added end game and cheat mode

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,19 +1,38 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+@@guesses_remaining = 5
 SECRET_NUMBER = rand(100)
 number = SECRET_NUMBER
 
 get '/' do
   guess = params["guess"]
+  if params['cheat'] == "true"
+    cheat_message = "The secret number is #{number}"
+  end
+
   if guess.to_s.empty? == true
     message = "Guess the secret number!"
-  else
+  elsif guess.to_i == number
+    message = "You win! Let's play again. Guess the secret number!"
+    game_end
+  elsif @@guesses_remaining > 1
     message = check_guess(guess, number)
+    @@guesses_remaining -=1
+    message_two = "#{@@guesses_remaining} guesses left."
+  else
+    message = "You lose! Oh well. Let's start over. Guess the secret number!"
+    game_end
   end
-  erb :index, :locals => {:number => number, :message => message}
+
+  erb :index, :locals => {:number => number, :message => message, :message_two => message_two, :cheat_message => cheat_message}
+
 end
 
+def game_end
+  number = rand(100)
+  @@guesses_remaining = 5
+end
 
 def check_guess(guess, number)
   if guess.to_i > (number + 5)
